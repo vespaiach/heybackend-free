13e039b81272ecb9c63443b297a8e3906e2caa38
Trinh Nguyen <nta.toan@gmail.com>
2026-04-16 15:32:29 -0400

    fix build error cont.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,8 @@
 import { redirect } from "next/navigation";
-import { getLoggedInTenant, getSession } from "@/lib/route-helpers";
+import { getLoggedInTenant } from "@/lib/route-helpers";
 
 export default async function Home() {
-  const session = await getSession();
-  const tenant = await getLoggedInTenant(session.user.id);
+  const tenant = await getLoggedInTenant();
 
   redirect(`/dashboard/${tenant.websites[0].id}/home`);
 }
